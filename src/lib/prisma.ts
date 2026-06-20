80e7be70dfd1bbe264d9ce388cf199b34a8fe154
sendy andreansah <sendy.lazada@gmail.com>
2026-06-21 02:16:16 +0700

    ini starter sakti

---
+++ src/lib/prisma.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@/generated/client";
+
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient;
+};
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
